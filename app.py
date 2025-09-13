97d4ddc2c4e765dcfebc1b5be09d96e8e9106032
Dheemanth49 <dheemanthdhanpal@gmail.com>
2025-09-13 14:44:39 +0530

    Add Render deployment configuration

--- app.py
+++ app.py
@@ -186,5 +186,7 @@ def forecast():
     return jsonify(forecast_data)
 
 if __name__ == '__main__':
+    import os
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
